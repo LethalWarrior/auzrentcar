4121ad4975b00e929eb07a20defbee9e51e323b4
LethalWarrior <albertfiraz@gmail.com>
2019-07-21 12:30:23 +0700

    Fix sidebar link for Super admin

--- application/views/layouts/sidebar/super.php
+++ application/views/layouts/sidebar/super.php
@@ -13,8 +13,8 @@
 <hr class="sidebar-divider my-0">
 
 <!-- Nav Item - Dashboard -->
-<li class="nav-item active">
-  <a class="nav-link" href="dashboard">
+<li class="nav-item <?php if($this->uri->segment(1) === 'dashboard')echo 'active'?>">
+  <a class="nav-link" href="<?php echo site_url('dashboard')?>">
     <i class="fas fa-fw fa-tachometer-alt"></i>
     <span>Dashboard</span></a>
 </li>
@@ -23,15 +23,15 @@
 <hr class="sidebar-divider">
 
 <!-- Nav Item - Users Collapse Menu -->
-<li class="nav-item">
+<li class="nav-item <?php if($this->uri->segment(1) === 'users')echo 'active'?>">
   <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUsers" aria-expanded="true" aria-controls="collapseUsers">
     <i class="fas fa-fw fa-user-alt"></i>
     <span>Users</span>
   </a>
   <div id="collapseUsers" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
     <div class="bg-white py-2 collapse-inner rounded">
-      <a class="collapse-item" href="users">View All</a>
-      <a class="collapse-item" href="users/create">Add User</a>
+      <a class="collapse-item" href="<?php echo site_url('users')?>">View All</a>
+      <a class="collapse-item" href="<?php echo site_url('users/create')?>">Add User</a>
     </div>
   </div>
 </li>
